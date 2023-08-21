adc5a9e81f7e212eb6e4e4713d13ec786cf2454f
brunoprosa <bruno.prosa01@gmail.com>
2023-08-21 17:39:02 -0300

    completo

--- src/app.js
+++ src/app.js
@@ -17,6 +17,8 @@ mongoClient.connect()
 	.then(() => db = mongoClient.db())
 	.catch((err) => console.log(err.message))
 
+setInterval(remocaoAutomatica, 15000)
+
 app.post("/participants", async (req, res) => {
     const {name} = req.body
     const userSchema = joi.object({
@@ -28,7 +30,7 @@ app.post("/participants", async (req, res) => {
         return res.status(422).send(errors);
     }
     try{
-    const exist = await db.collection("participants").find(name).toArray()
+    const exist = await db.collection("participants").find({name}).toArray()
     if (exist.length === 1) return res.status(409).send("nome já existente!")
     await db.collection("participants").insertOne({
         name,
@@ -47,11 +49,13 @@ app.post("/participants", async (req, res) => {
         return res.status(500).send(err.message);
     }
 });
+
 app.get("/participants", (req, res) => {
     db.collection("participants").find().toArray()
     .then(participants => res.send(participants))
     .catch(err => res.status(500).send(err.message))
 })
+
 app.post("/messages", async (req, res) => {
     const from = req.headers.user
     const {to, text, type} = req.body
@@ -66,7 +70,7 @@ app.post("/messages", async (req, res) => {
         return res.status(422)
     }
     try{
-        const exist = await db.collection("participants").find(from).toArray()
+        const exist = await db.collection("participants").find({name: from}).toArray()
         if(exist.length === 0) return res.status(422).send("remetente inexistente!")
         const time = dayjs().format("HH:mm:ss")
         await db.collection("messages").insertOne({
@@ -77,6 +81,77 @@ app.post("/messages", async (req, res) => {
         return res.status(500).send(err.message);
     }
 })
+
+app.get("/messages", async (req, res) => {
+    const user = req.headers.user
+    const limit = parseInt(req.query.limit)
+    if(limit <= 0) return res.status(422).send("limite inválido")
+    try{
+        const messages = await db.collection("messages").find({$or: [
+            {type: "message"}, 
+            {to: "Todos"}, 
+            {type: "private_message", to: user},
+            {type: "private_message", from: user}
+        ]}).toArray()
+        if(limit === undefined) return res.send(messages)
+        let aux = []
+        for(let i = messages.length - limit; i < messages.length; i++){
+            aux.push(messages[i])
+        }
+        return res.send(aux)
+    } catch(err){
+        return res.status(500).send(err.message);
+    }
+})
+
+app.post("/status", async (req,res) => {
+    const name = req.headers.user
+    if (!name) return res.status(404)
+    try{
+        const exist = await db.collection("participants").find({name}).toArray()
+        if(exist.length === 0) return res.status(404)
+        const newStatus = {
+            name,
+            lastStatus: Date.now()
+        }
+        await db.collection("participants").updateOne(
+        { name }, { $set: newStatus }
+        )
+        res.status(200)
+    } catch(err){
+        return res.status(500).send(err.message);
+    }
+})
+
+async function remocaoAutomatica(){
+    let filtro = Date.now() - 10000
+    try{
+        const removidos = await db.collection("participants").find(
+            {lastStatus: {$lt: filtro}}
+        ).toArray()
+        removidos.map( rem => remover(rem))
+        await db.collection("participants").deleteMany(
+            {lastStatus: {$lt: filtro}}
+        )
+    }catch (err){
+        return res.status(500).send(err.message);
+    }
+}
+
+async function remover(rem){
+    try{
+        const time = dayjs().format("HH:mm:ss")
+        await db.collection("messages").insertOne({ 
+            from: rem.name,
+            to: 'Todos',
+            text: 'sai da sala...',
+            type: 'status',
+            time
+        })
+    }catch (err){
+        return res.status(500).send(err.message);
+    }
+}
 
 const PORT = 5000
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`))
